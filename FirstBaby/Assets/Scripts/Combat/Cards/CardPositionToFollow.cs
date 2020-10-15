90e50aa4533fdf6a84836d76a0f9fa0ff489dfaa
phtriplox <phtriplox8090@gmail.com>
2020-10-15 14:08:30 -0300

    scripts commented

--- FirstBaby/Assets/Scripts/Combat/Cards/CardPositionToFollow.cs
+++ FirstBaby/Assets/Scripts/Combat/Cards/CardPositionToFollow.cs
@@ -7,7 +7,7 @@ public class CardPositionToFollow
     public Vector3 position;
     public Quaternion rotation;
 
-    public CardPositionToFollow(Vector3 pos, Quaternion rot)
+    public CardPositionToFollow(Vector3 pos, Quaternion rot) //this is the position entity that a card in hand will follow for it's movement
     {
         position = pos;
         rotation = rot;
