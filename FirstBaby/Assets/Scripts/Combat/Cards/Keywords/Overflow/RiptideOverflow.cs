2eb0710c27aef21dc68a35c291c4aa678bcde9ef
rdss33 <raphaeldssoares@gmail.com>
2020-11-07 17:30:04 -0400

    New Card: Riptide
    Fiz a carta Riptide e tirei o erro do Unstable  e do inimigo spawnando por cima do outro

---
+++ FirstBaby/Assets/Scripts/Combat/Cards/Keywords/Overflow/RiptideOverflow.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RiptideOverflow : OverflowKeyword
+{
+    [SerializeField] private int ExtraDamage = 5;
+    public override void OverflowEffect()
+    {
+        myCard.PhysicalCardBehaviour.AddValue += ExtraDamage;// Increases this card's damage
+    }
+}
