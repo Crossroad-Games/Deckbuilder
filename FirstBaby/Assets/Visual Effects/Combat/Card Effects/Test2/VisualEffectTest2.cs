7bb5d2472042db732b259bd5628691cae41374d0
phtriplox <phtriplox8090@gmail.com>
2020-11-14 20:05:17 -0300

    Visual effect test

---
+++ FirstBaby/Assets/Visual Effects/Combat/Card Effects/Test2/VisualEffectTest2.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisualEffectTest2 : MonoBehaviour
+{
+    public AlchemistFireCard card;
+    public Transform targetTransform;
+    private Animator anim;
+    private bool moveToTarget;
+    public float orbMovementSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(moveToTarget)
+        {
+            transform.position = Vector3.Lerp(transform.position, targetTransform.position, orbMovementSpeed * Time.deltaTime); // Move the orb to enemy position
+            if((transform.position - targetTransform.position).magnitude <= 0.2f)
+            {
+                //card.DealEffect();
+                anim.SetTrigger("ActivateExplosion");
+            }
+        }
+    }
+
+    public void beginMovement()
+    {
+        moveToTarget = true;
+    }
+
+    public void ByeBye()
+    {
+        Destroy(this.gameObject);
+    }
+}
