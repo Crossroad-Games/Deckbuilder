83b8e675c3d119907dc4fdd895e973117ec09cc3
rdss33 <raphaeldssoares@gmail.com>
2020-11-26 00:13:30 -0400

    Card Texts: Rework
    Refiz todos os textos de carta pra padronizar o tamanho das letras, posições, fontes...

--- FirstBaby/Assets/Scripts/Exploration/Objects/Interactable/LightMirror.cs
+++ FirstBaby/Assets/Scripts/Exploration/Objects/Interactable/LightMirror.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class LightMirror : Interactable
 {
     private List<Button> ShownCards= new List<Button>();
+    [SerializeField] private float Test;
     private CardsGallery Gallery;// Reference to the UI Manager's Gallery component will allow us to expose the cards currently on the player's possession to choose one to level up
     public override void Awake()
     {
@@ -16,6 +18,7 @@ public class LightMirror : Interactable
     {
         Player.GetComponent<PlayerMovement>().canMove = false; //Disable player movement
         Gallery.ShowDeckGallery(Player.GetComponent<DungeonPlayer>().myData.CardCollectionID);// Enables the gallery 
+        GameObject.Find("Dungeon Canvas").transform.Find("Deck Gallery").transform.Find("Bottom Text").GetComponent<TMP_Text>().text = "Choose a card to upgrade ALL of its copies";
         foreach (GameObject Card in Gallery.cardsDisplayedAsButtons.Keys)// Cycle through each displayed card
             if (Card != null)// If not null
                 ShownCards.Add(Gallery.cardsDisplayedAsButtons[Card]);// Access the button linked to this gameobject and save it
@@ -29,6 +32,7 @@ public class LightMirror : Interactable
     }
     public void ChosenCard(int CardID)// Method that will handle the lvl up event
     {
+        GameObject.Find("Dungeon Canvas").transform.Find("Deck Gallery").transform.Find("Bottom Text").GetComponent<TMP_Text>().text = string.Empty;
         Player.GetComponent<DungeonPlayer>().myData.CardLevels[CardID]++;// Increases the LVL of this card
         Gallery.CloseCollectionGallery();// Close the gallery
         Player.GetComponent<PlayerMovement>().canMove = true; //Disable player movement
